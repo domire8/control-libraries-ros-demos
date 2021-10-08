062d9e07c07295c85a498c373b9c1237d2a5bbc1
Dominic Reber <dominic.reber@epfl.ch>
2021-10-08 16:59:43 +0200

    Add state machine for throwing

--- ros_examples/scripts/mobile_manipulator_control.cpp
+++ ros_examples/scripts/mobile_manipulator_control.cpp
@@ -6,9 +6,8 @@
 #include <robot_model/Model.hpp>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
-#include <state_representation/space/cartesian/CartesianPose.hpp>
-#include <state_representation/robot/Jacobian.hpp>
 #include <std_msgs/Float64MultiArray.h>
+#include <std_srvs/Trigger.h>
 #include <tf2_ros/transform_listener.h>
 
 using namespace state_representation;
@@ -18,13 +17,31 @@ using namespace robot_model;
 using namespace std::chrono_literals;
 
 namespace {
+
+static CartesianPose from_tf_message(const geometry_msgs::TransformStamped& tf) {
+  auto transform = CartesianPose(tf.child_frame_id, tf.header.frame_id);
+  transform.set_position(
+      tf.transform.translation.x, tf.transform.translation.y, tf.transform.translation.z
+  );
+  transform.set_orientation(
+      std::vector<double>{
+          tf.transform.rotation.w, tf.transform.rotation.x, tf.transform.rotation.y, tf.transform.rotation.z
+      }
+  );
+  return transform;
+}
+
+enum STATE {
+  APPROACH_1, PICK_BALL, APPROACH_2, APPROACH_3, RELEASE, GO_BACK
+};
+
 class RobotInterface {
 private:
+  ros::NodeHandle* nh_;
   ros::Subscriber odom_subscriber_;
   ros::Subscriber joint_state_subscriber_;
   ros::Publisher base_twist_publisher_;
   ros::Publisher arm_velocity_publisher_;
-  tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
 
   Model robot_;
@@ -33,11 +50,12 @@ private:
   CartesianPose base_to_arm_;
 
 public:
+  tf2_ros::Buffer tf_buffer;
   bool odom_received = false;
   bool joint_state_received = false;
 
   explicit RobotInterface(ros::NodeHandle* node_handle, const std::string& urdf_path) :
-      tf_listener_(tf_buffer_), robot_("franka", urdf_path) {
+      nh_(node_handle), tf_listener_(tf_buffer), robot_("franka", urdf_path) {
     this->base_state_ = CartesianState("robotbase_footprint", "odom");
     this->odom_subscriber_ = node_handle->subscribe("/robot/odom", 10, &RobotInterface::odom_callback, this);
     this->joint_state_subscriber_ =
@@ -89,21 +107,25 @@ private:
   bool get_transform(CartesianPose& transform, const std::string& target, const std::string& source) {
     geometry_msgs::TransformStamped tf;
     try {
-      tf = this->tf_buffer_.lookupTransform(target, source, ros::Time(0));
+      tf = this->tf_buffer.lookupTransform(target, source, ros::Time(0));
     } catch (tf2::TransformException& ex) {
-      ROS_WARN("%s", ex.what());
       return false;
     }
-    transform = CartesianPose(target, source);
-    transform.set_position(
-        tf.transform.translation.x, tf.transform.translation.y, tf.transform.translation.z
-    );
-    transform.set_orientation(
-        std::vector<double>{
-            tf.transform.rotation.w, tf.transform.rotation.x, tf.transform.rotation.y, tf.transform.rotation.z
-        }
-    );
+    transform = from_tf_message(tf);
     return true;
+  }
+
+  bool service_client(const std::string& name) {
+    ros::ServiceClient client = this->nh_->serviceClient<std_srvs::Trigger>(name);
+    std_srvs::Trigger srv;
+    if (client.call(srv)) {
+      if (srv.response.success) {
+        return true;
+      }
+      ROS_ERROR_STREAM(srv.response.message);
+      return false;
+    }
+    return false;
   }
 
 public:
@@ -118,7 +140,7 @@ public:
     this->base_twist_publisher_.publish(msg);
   }
 
-  void publish_arm_command(const JointVelocities& command) {
+  void publish_arm_command(const JointTorques& command) {
     std_msgs::Float64MultiArray msg;
     msg.data = command.to_std_vector();
     this->arm_velocity_publisher_.publish(msg);
@@ -138,35 +160,147 @@ public:
     return pose;
   }
 
+  Jacobian get_jacobian() {
+    auto jac = this->robot_.compute_jacobian(this->arm_joint_state_);
+    return Jacobian(jac.get_name(), jac.get_frame(), jac.data(), "panda_link0");
+  }
+
   JointVelocities inverse_velocity(const CartesianTwist& twist) {
     return robot_.inverse_velocity(twist, this->arm_joint_state_);
+  }
+
+  bool pick_ball() {
+    return this->service_client("/ball/attach");
+  }
+
+  bool release() {
+    return this->service_client("/ball/let_go");
+  }
+
+  bool lock_joints() {
+    return this->service_client("/arm/lock_joints");
   }
 
 };
 }
 
 void control_loop(RobotInterface& robot, const int& freq) {
+  ros::Rate rate(freq);
+  CartesianPose base_to_arm_base;
+  while (!robot.get_base_to_arm_transform(base_to_arm_base)) {
+    rate.sleep();
+  }
+
+  CartesianTwistController ctrl(5, 5, .5, .5);
+
   // set a desired target and a linear ds toward the target for the base
   CartesianPose base_target("robotbase_footprint", "odom");
-  base_target.set_position(5, 0, 0);
+  base_target.set_position(-1.3, 0, 0);
   Linear<CartesianState> base_ds(base_target, {50.0, 50.0, 0, 0, 0, 10.0});
+  double base_max_vel = 0.4;
 
   // set a desired target and a linear ds toward the target for the arm
   CartesianPose arm_target("panda_link8", "panda_link0");
-  arm_target.set_position(0.2, 0.5, 0.5);
+  arm_target.set_position(0.5, 0, 0.5);
   arm_target.set_orientation(Eigen::Quaterniond(0, 1, 0, 0));
   Linear<CartesianState> arm_ds(arm_target, {50.0, 50.0, 50.0, 10.0, 10.0, 10.0});
+  double arm_max_lin = 0.4;
+  double arm_max_ang = 1;
 
-  ros::Rate rate(freq);
+  int wait = 0;
+  bool received = false;
+  STATE state = STATE::APPROACH_1;
   while (ros::ok()) {
     if (robot.odom_received && robot.joint_state_received) {
+      switch (state) {
+        case STATE::APPROACH_1 : {
+          geometry_msgs::TransformStamped tf_msg;
+          try {
+            tf_msg = robot.tf_buffer.lookupTransform("panda_link0", "ball", ros::Time(0));
+            received = true;
+          } catch (tf2::TransformException& ex) {
+            received = false;
+          }
+          if (received) {
+            auto ball_in_arm_base = from_tf_message(tf_msg);
+            ball_in_arm_base.set_orientation(arm_ds.get_attractor().get_orientation());
+            if (robot.get_eef_pose().dist(ball_in_arm_base, CartesianStateVariable::POSITION) < 0.07
+                && robot.get_base_pose().dist(base_ds.get_attractor(), CartesianStateVariable::POSITION) < 0.01) {
+              if (wait < 1000) {
+                ++wait;
+              } else {
+                wait = 0;
+                received = false;
+                state = STATE::PICK_BALL;
+              }
+            }
+            if (ball_in_arm_base.get_position().norm() > 1) {
+              ball_in_arm_base.set_position(ball_in_arm_base.get_position().normalized());
+            }
+            ball_in_arm_base.set_position(ball_in_arm_base.get_position() - Eigen::Vector3d(0, 0, -0.06));
+            arm_ds.set_attractor(ball_in_arm_base);
+          }
+        }
+          break;
+        case STATE::PICK_BALL: {
+          for (std::size_t i = 0; i < 3000; ++i) {
+            break;
+          }
+          if (robot.pick_ball()) {
+            state = STATE::APPROACH_2;
+            base_max_vel = 0.2;
+            base_target.set_position(0, 0, 0);
+            base_ds.set_attractor(base_target);
+//            arm_max_lin = 1;
+//            arm_max_ang = 1.5;
+//            arm_ds.set_gain({200.0, 200.0, 200.0, 50.0, 50.0, 50.0});
+            arm_target.set_position(0.5, -0.4, 1.0 + base_to_arm_base.get_position().z());
+            arm_target.set_orientation(Eigen::Quaterniond(0.707, 0.707, 0, 0));
+            arm_ds.set_attractor(arm_target);
+          }
+        }
+          break;
+        case STATE::APPROACH_2: {
+          if (robot.get_eef_pose().dist(arm_ds.get_attractor(), CartesianStateVariable::POSE) < 0.02) {
+            robot.lock_joints();
+            state = APPROACH_3;
+            base_max_vel = 1.5;
+            base_target.set_position(2, 0, 0);
+            base_ds.set_attractor(base_target);
+          }
+        }
+          break;
+        case STATE::APPROACH_3: {
+          arm_ds.set_attractor(robot.get_eef_pose());
+          if (abs(robot.get_base_pose().get_position().x() - 1.3) < 0.05) {
+            state = STATE::RELEASE;
+          }
+        }
+          break;
+        case STATE::RELEASE: {
+          if (base_max_vel < 0.01) {
+            base_target.set_position(0, 0 ,0);
+            base_ds.set_attractor(base_target);
+            base_max_vel = 0.4;
+            state = STATE::GO_BACK;
+          }
+          else if (robot.release()) {
+            base_max_vel = 0;
+          }
+        }
+          break;
+        case STATE::GO_BACK:
+          break;
+      }
       CartesianTwist twist = base_ds.evaluate(robot.get_base_pose());
-      twist.clamp(0.25, 0.5);
+      twist.clamp(base_max_vel, 0);
       robot.publish_base_twist(twist);
 
-      twist = arm_ds.evaluate(robot.get_eef_pose());
-      twist.clamp(0.25, 0.5);
-      robot.publish_arm_command(robot.inverse_velocity(twist));
+      auto ee_pose = robot.get_eef_pose();
+      twist = arm_ds.evaluate(ee_pose);
+      twist.clamp(arm_max_lin, arm_max_ang);
+//      robot.publish_arm_command(robot.inverse_velocity(twist));
+      robot.publish_arm_command(ctrl.compute_command(twist, ee_pose, robot.get_jacobian()));
       rate.sleep();
     }
     ros::spinOnce();
